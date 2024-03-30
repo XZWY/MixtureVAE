f7385b4dfb9a219422281e7cd57fa1e1de8fb2e6
zhongweiyangxu <zx21@illinois.edu>
2024-03-29 22:26:47 -0700

    add subband model

--- start_sb.sh
+++ start_sb.sh
@@ -1,14 +1,14 @@
 export PYTHONPATH=/data/romit/alan/MixtureVAE
 
-export CUDA_VISIBLE_DEVICES=6,7
+export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
 musdb_root_path='/data/romit/alan/musdb18'
 vocalset_root_path='/data/romit/alan/vocalset11'
-config_path="/data/romit/alan/MixtureVAE/config_SourceVAE.json"
+config_path="/data/romit/alan/MixtureVAE/config_SourceVAESB.json"
 # config_path="/ws/ifp-54_2/hasegawa/xulinf2/MixtureVAE/log_files/log_test_vae_kl_codec_100_256bn/config.json"
-log_root="/data/romit/alan/MixtureVAE/log_files/log_test_vae_kl_codec_10_128bn"
+log_root="/data/romit/alan/MixtureVAE/log_files/log_test_sb_100_64bn"
 
 echo "Train model..."
-python3 train_source_vae_kl_codec.py \
+python3 train_source_vae_sb_kl_codec.py \
     --musdb_root ${musdb_root_path}\
     --vocalset_root ${vocalset_root_path}\
     --config ${config_path} \
